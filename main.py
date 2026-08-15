c8cc48b21460c1efdfe9c2f6fe308ba272d6eb31
Matheus Henrique Costa Durante <matheusdurante14@hotmail.com>
2026-08-15 12:41:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# 217.0.0.1:8000/
 from fastapi import FastAPI
 import random
 
 app = FastAPI()
+# 217.0.0.1:8000/
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 # 217.0.0.1:8000/teste
-@app.get("/teste1")
+@app.get("/teste")
 async def root():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
